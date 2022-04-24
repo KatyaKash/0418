03ce4beec25927343acdf0f2f0a92c182a1350ea
KatyaKash <katya_kash@inbox.ru>
2022-04-24 23:29:46 +0700

    94

--- project/config/routes.php
+++ project/config/routes.php
@@ -11,8 +11,9 @@
 		new Route('/test/:var1/:var2/', 'page', 'act'),
 		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
 		new Route('/page/:id/', 'page', 'show'),
-		new Route('/user/:id/', 'user', 'show'),
-
-		
+		new Route('/user/:id/:key/', 'user', 'info'),
+		new Route('/user/all/', 'user', 'all'),
+		new Route('/user/first/:n/', 'user', 'first'),
+	
 	];
 	
